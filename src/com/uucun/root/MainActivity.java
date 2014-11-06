2335e674c2ce2851acdb23cf43d97e3732ccb521
hewu2008 <hewu2008@gmail.com>
2014-11-06 14:26:00 +0800

    格式化代码，增加临时root功能

--- src/com/uucun/root/MainActivity.java
+++ src/com/uucun/root/MainActivity.java
@@ -7,6 +7,7 @@ import java.util.List;
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.os.RemoteException;
@@ -19,14 +20,11 @@ import android.widget.Toast;
 
 import com.qihoo.appstore.R;
 import com.qihoo.constant.Constants;
-import com.qihoo.event.InstallFinishEvent;
-import com.qihoo.event.ServiceStartEvent;
+import com.qihoo.permmgr.LocalRoot;
 import com.qihoo.rtservice.IRTServiceImpl;
 import com.qihoo.rtservice.IRootService;
 import com.qihoo.rtservice.Utils;
 import com.qihoo.utils.FileUtils;
-
-import de.greenrobot.event.EventBus;
 
 /**
  * Copyright (C) 2014-2015 hewu <hewu2008@gmail.com>
@@ -56,16 +54,16 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 		initView();
 		setListener();
-		startRootService();
+		new RootAsyncTask().execute();
 	}
-	
+
 	private void initView() {
-		mStatusText = (TextView)findViewById(R.id.tv_status);
-		mBrowerApkFileBtn = (Button)findViewById(R.id.btn_browser_apk_file);
+		mStatusText = (TextView) findViewById(R.id.tv_status);
+		mBrowerApkFileBtn = (Button) findViewById(R.id.btn_browser_apk_file);
 		mBrowerApkFileBtn.setVisibility(View.GONE);
 		mStatusText.setText("status:\n");
 	}
-	
+
 	private void setListener() {
 		mBrowerApkFileBtn.setOnClickListener(new View.OnClickListener() {
 			@Override
@@ -74,72 +72,52 @@ public class MainActivity extends Activity {
 				if (rtService != null) {
 					showFileChooser();
 				} else {
-					Toast.makeText(MainActivity.this, 
-						"qh_root_service process not started.", Toast.LENGTH_SHORT).show();
-				} 
+					Toast.makeText(MainActivity.this,
+							"root process not started.", Toast.LENGTH_SHORT)
+							.show();
+				}
 			}
 		});
-		EventBus.getDefault().register(this);
 	}
 	
-	private void startRootService() {
+	private int doRoot() {
+		LocalRoot root = LocalRoot.getInstance();
+		return root.doRoot(this);
+	}
+
+	private void startRootProcess() {
 		if (Utils.hasSuCmd() && getRTService() == null) {
-			mStatusText.append("starting qh_root_service process, please wait...\n");
-			new Thread() {
-				@Override 
-				public void run() {
-					List<String> commands = new ArrayList<String>();
-					String exportCmd = "export CLASSPATH=$CLASSPATH:" + MainActivity.this.getPackageCodePath();
-					commands.add(exportCmd);
-					String servicePath = IRTServiceImpl.class.getName();
-					String startProcessCmd = String.format("app_process /system/bin %s --nice-name=%s --application &", 
-						servicePath, IRTServiceImpl.SERVICE_NAME);
-					commands.add(startProcessCmd);
-					ServiceStartEvent event = new ServiceStartEvent();
-					String result = Utils.execP(commands);
-					event.setErrorMessage(result);
-					EventBus.getDefault().post(event);
-				}
-			}.start();
-		}else if (getRTService() != null) {
+			mStatusText.append("starting root process, please wait...\n");
+			new StartProcessAsyncTask().execute();
+		} else if (getRTService() != null) {
 			mBrowerApkFileBtn.setVisibility(View.VISIBLE);
-			mStatusText.append("qh_root_service already started.\n");
+			mStatusText.append("root process already started.\n");
 		} else {
-			mStatusText.append("this device does not supported!\n");
+			mStatusText.append("does not supported this device.\n");
 		}
 	}
-	
-	public void onEventMainThread(ServiceStartEvent event) {
-		if ("success".equals(event.getErrorMessage())) {
-			mBrowerApkFileBtn.setVisibility(View.VISIBLE);
-			mStatusText.append("call qh_root_service command success!\n");
-		}else {
-			mStatusText.append("call qh_root_service command failed!\n");
-		}
-    }
-	
-	public void onEventMainThread(InstallFinishEvent event) {
-		mStatusText.append("install result: " + event.getResult() + "\n");
-    }
-	
+
 	public static IRootService getRTService() {
 		IBinder binder = ServiceManager.getService(IRTServiceImpl.SERVICE_NAME);
 		if (binder != null)
 			return IRootService.Stub.asInterface(binder);
 		return null;
 	}
-	
+
 	private void showFileChooser() {
-	    Intent intent = new Intent(Intent.ACTION_GET_CONTENT); 
-	    intent.setType("application/vnd.android.package-archive"); 
-	    intent.addCategory(Intent.CATEGORY_OPENABLE);
-	    try {
-	        startActivityForResult(Intent.createChooser(intent, "select apk file"), FILE_SELECT_CODE);
-	    } catch (android.content.ActivityNotFoundException ex) {
-	        Toast.makeText(this, "Please install a File Manager.",  Toast.LENGTH_SHORT).show();
-	    }
+		Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+		intent.setType("application/vnd.android.package-archive");
+		intent.addCategory(Intent.CATEGORY_OPENABLE);
+		try {
+			startActivityForResult(
+					Intent.createChooser(intent, "select apk file"),
+					FILE_SELECT_CODE);
+		} catch (android.content.ActivityNotFoundException ex) {
+			Toast.makeText(this, "Please install a File Manager.",
+					Toast.LENGTH_SHORT).show();
+		}
 	}
-	
+
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		switch (requestCode) {
@@ -148,31 +126,90 @@ public class MainActivity extends Activity {
 				Uri uri = data.getData();
 				final String path = FileUtils.getPath(this, uri);
 				Log.d(Constants.TAG, "path:" + path);
-				if (path == null) return;
-				final IRootService rtService = getRTService();
+				if (path == null)
+					return;
 				File file = new File(path);
+				IRootService rtService = getRTService();
 				if (file.exists() == true && rtService != null) {
-					mStatusText.append("installing...\n");
-					new Thread() {
-						@Override
-						public void run() {
-							try {
-								boolean bRet = rtService.install(path);
-								InstallFinishEvent event = new InstallFinishEvent();
-								event.setResult(String.valueOf(bRet));
-								EventBus.getDefault().post(event);
-							} catch (RemoteException e) {
-								e.printStackTrace();
-								InstallFinishEvent event = new InstallFinishEvent();
-								event.setResult(e.getMessage());
-								EventBus.getDefault().post(event);
-							}
-						}
-					}.start();
+					new InstallPackageAsyncTask().execute(path);
 				}
 			}
 			break;
 		}
 		super.onActivityResult(requestCode, resultCode, data);
+	}
+
+	private class RootAsyncTask extends AsyncTask<Void, Void, Integer> {
+		@Override
+		protected void onPreExecute() {
+			mStatusText.append("do Rooting...\n");
+		}
+
+		@Override
+		protected Integer doInBackground(Void... params) {
+			return doRoot();
+		}
+
+		@Override
+		protected void onPostExecute(Integer result) {
+			mStatusText.append("do Rooting result: " + result + ".\n");
+			startRootProcess();
+		}
+	}
+
+	private class StartProcessAsyncTask extends AsyncTask<Void, Void, String> {
+		@Override
+		protected String doInBackground(Void... params) {
+			List<String> commands = new ArrayList<String>();
+			String exportCmd = "export CLASSPATH=$CLASSPATH:"
+					+ MainActivity.this.getPackageCodePath();
+			commands.add(exportCmd);
+			String servicePath = IRTServiceImpl.class.getName();
+			String startProcessCmd = String
+					.format("app_process /system/bin %s --nice-name=%s --application &",
+							servicePath, IRTServiceImpl.SERVICE_NAME);
+			commands.add(startProcessCmd);
+			return Utils.execP(commands);
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			if ("success".equals(result)) {
+				mBrowerApkFileBtn.setVisibility(View.VISIBLE);
+				mStatusText.append("start root process success\n");
+			} else {
+				mStatusText.append("start root process failed\n");
+			}
+		}
+	}
+
+	private class InstallPackageAsyncTask extends
+			AsyncTask<String, Void, String> {
+		@Override
+		protected void onPreExecute() {
+			mStatusText.append("installing...\n");
+		}
+
+		@Override
+		protected String doInBackground(String... params) {
+			IRootService rtService = getRTService();
+			boolean bRet = false;
+			try {
+				bRet = rtService.install(params[0]);
+			} catch (RemoteException e) {
+				e.printStackTrace();
+				return e.getMessage();
+			}
+			if (bRet == true) {
+				return "success";
+			} else {
+				return "failed";
+			}
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			mStatusText.append("install result: " + result + "\n");
+		}
 	}
 }
